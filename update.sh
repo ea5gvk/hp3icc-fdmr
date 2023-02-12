0ebbd69843ecabc8eac7122bd5c3fe848570e729
RaspbianProyect by HP3ICC <setcom40@gmail.com>
2023-02-12 13:45:28 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,108 @@
+sudo cat > /bin/menu-update <<- "EOF"
 #!/bin/bash
+while : ; do
+choix=$(whiptail --title "Raspbian Proyect HP3ICC / update FDMR-Monitor OA4DOA" --menu "Nota Importante: antes de iniciar la actualizacion, el proceso de actualizacion borrara por completo todas las configuraciones, al finalizar la actualizacion el servicio se reinniciara automaticamente.
+" 17 50 4 \
+1 " Update FreeDMR " \
+2 " Update FDMR-Monitor  " \
+3 " Menu Principal " 3>&1 1>&2 2>&3)
+exitstatus=$?
+#on recupere ce choix
+#exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    echo "Your chosen option:" $choix
+else
+    echo "You chose cancel."; break;
+fi
+# case : action en fonction du choix
+case $choix in
+1)
+menu-up-fdm ;;
+2)
+menu-up-fdmon ;;
+3)
+break;
+esac
+done
+exit 0
+
+
+EOF
+#
+
+sudo cat > /bin/menu-up-fdm <<- "EOF"
+#!/bin/bash
+while : ; do
+choix=$(whiptail --title "Raspbian Proyect HP3ICC / update FreeDMR" --menu "Nota Importante: debe debe agregar todos sus obp en la opcion numero uno, ( 1-Lista de OBP )antes de iniciar la actualizacion, el proceso de actualizacion borrara por completo la carpeta /opt/FreeDMR, al finalizar la actualizacion el servicio Freedmr se reinniciara automaticamente.
+" 18 55 5 \
+1 " List OBP " \
+2 " List Rules  " \
+3 " shell extra " \
+4 " Iniciar Actualizacion FreeDMR  " \
+5 " Menu Principal " 3>&1 1>&2 2>&3)
+exitstatus=$?
+#on recupere ce choix
+#exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    echo "Your chosen option:" $choix
+else
+    echo "You chose cancel."; break;
+fi
+# case : action en fonction du choix
+case $choix in
+1)
+sudo nano /opt/obp.txt ;;
+2)
+sudo nano /opt/rules.txt ;;
+3)
+sudo nano /opt/extra-1.sh && chmod +x /opt/extra* ;;
+4)
+sh /opt/fdmr-update.sh ;;
+5)
+break;
+esac
+done
+exit 0
+EOF
+#
+#
+sudo cat > /bin/menu-up-fdmon <<- "EOF"
+#!/bin/bash
+while : ; do
+choix=$(whiptail --title "Raspbian Proyect HP3ICC / update FDMR-Monitor OA4DOA" --menu "Nota Importante: antes de iniciar la actualizacion, el proceso de actualizacion borrara por completo todas las configuraciones, al finalizar la actualizacion el servicio se reinniciara automaticamente.
+" 17 50 4 \
+1 " shell extra " \
+2 " Iniciar Actualizacion FDMR-Monitor  " \
+3 " Menu Principal " 3>&1 1>&2 2>&3)
+exitstatus=$?
+#on recupere ce choix
+#exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    echo "Your chosen option:" $choix
+else
+    echo "You chose cancel."; break;
+fi
+# case : action en fonction du choix
+case $choix in
+1)
+sudo nano /opt/extra-2.sh && chmod +x /opt/extra* ;;
+2)
+sh /opt/monitor-update.sh ;;
+3)
+break;
+esac
+done
+exit 0
+
+
+EOF
+##########################
+wget https://gitlab.com/hp3icc/fdmr/-/raw/main/fdmr-update.sh -O /opt/fdmr-update.sh
+wget https://gitlab.com/hp3icc/fdmr/-/raw/main/fdmr-update.sh -O /opt/monitor-update.sh
+#######
+sudo chmod +x /opt/fdmr-update.sh
+sudo chmod +x /opt/monitor-update.sh
+sudo chmod +x /bin/menu*
+sudo chmod +x /bin/MENU*
+menu-update
+
