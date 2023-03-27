2d0dc9519372c6c3d61de280140541f73c38a173
RaspbianProyect by HP3ICC <setcom40@gmail.com>
2023-03-27 15:00:41 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -336,6 +336,11 @@ mkdir /var/log/FreeDMR
 chmod +x /opt/FreeDMR/install.sh
 ./install.sh
 #
+if [ "$(cat /opt/FreeDMR/FreeDMR-SAMPLE.cfg | grep 'TOPO_FILE')" != "" ]; then
+sudo sed -i 's/TOPO_FILE:.*/TOPO_FILE: topography.json/' /opt/FreeDMR/FreeDMR-SAMPLE.cfg
+else
+sed '45 a TOPO_FILE: topography.json' -i /opt/FreeDMR/FreeDMR-SAMPLE.cfg 
+fi
 sudo sed -i "s/ANNOUNCEMENT_LANGUAGE:.*/ANNOUNCEMENT_LANGUAGE: es_ES/g" /opt/FreeDMR/FreeDMR-SAMPLE.cfg
 sudo sed -i "s/SINGLE_MODE:.*/SINGLE_MODE: False/g" /opt/FreeDMR/FreeDMR-SAMPLE.cfg
 sudo sed -i "s/VOICE_IDENT:.*/VOICE_IDENT: False/g"  /opt/FreeDMR/FreeDMR-SAMPLE.cfg
